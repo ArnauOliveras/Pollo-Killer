71107cff9d43cfdac59912afe26e6087cf72fc56
ArnauOliveras <aoliverasf@edu.tecnocampus.cat>
2021-06-04 11:47:12 +0200

    Inicio

---
+++ App/Source/Game/Aim.cs
@@ -0,0 +1,30 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+
+namespace TcGame
+{
+    public class Aim : StaticActor
+    {
+        private Texture texture;
+        //private Vector2f WeaponAim1, WeaponAim2;
+
+        public Aim()
+        {
+            //Layer = ELayer.Back;
+
+            //texture = Resources.Texture("Textures/Mira.png");
+            //texture.Repeated = true;
+            //Sprite = new Sprite(texture);
+            //Position = new Vector2f(1024, 768) / 2;
+            //Center();
+
+        }
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+            Vector2f movimiento = new Vector2f(0.0f, 0.0f);
+        }
+    }
+}
--- App/Source/Game/WeaponAim.cs
+++ App/Source/Game/WeaponAim.cs
@@ -1,7 +1,33 @@
-﻿namespace TcGame
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+
+namespace TcGame
 {
-  public class WeaponAim : StaticActor
-  {
-    
-  }
+    public class WeaponAim : StaticActor
+    {
+        private Texture texture;
+        private Vector2f WeaponAimOffset1, WeaponAimOffset2;
+
+        public WeaponAim()
+        {
+            WeaponAimOffset1 = new Vector2f(30.0f, -30.0f);
+            WeaponAimOffset2 = new Vector2f(-40.0f, -30.0f);
+
+            Aim WeaponAim1 = MyGame.Instance.Scene.Create<Aim>();
+            WeaponAim1.Position = Position + WeaponAimOffset1;
+            WeaponAim1.Rotation = Rotation;
+
+            Aim WeaponAim2 = MyGame.Instance.Scene.Create<Aim>();
+            WeaponAim2.Position = Position + WeaponAimOffset2;
+            WeaponAim2.Rotation = Rotation;
+
+        }
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+            Vector2f movimiento = new Vector2f(0.0f, 0.0f);
+        }
+    }
 }
