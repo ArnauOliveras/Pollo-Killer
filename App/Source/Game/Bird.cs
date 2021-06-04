5ffe129381c7534b8a18fe013b58c986ed42fd89
Hodei Otegi González <otegih@gmail.com>
2021-06-04 12:22:50 +0200

    Bird and explosion

--- App/Source/Game/Bird.cs
+++ App/Source/Game/Bird.cs
@@ -1,6 +1,28 @@
-﻿namespace TcGame
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+
+namespace TcGame
 {
-  public class Bird : Enemy
-  {
-  }
+    public class Bird : Enemy
+    {
+        private Texture texture;
+
+        public Bird()
+        {
+            texture = Resources.Texture("Textures/Enemies/Bird");
+            texture.Repeated = true;
+            AnimatedSprite = new AnimatedSprite(texture, 4, 1);
+            AnimatedSprite.Loop = true;
+            AnimatedSprite.FrameTime = 0.1f;
+            Position = new Vector2f(1024, 768) / 2;
+            Center();
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+        }
+    }
 }
