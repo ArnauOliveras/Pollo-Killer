3a46f314437991e9b1f93d5cb51406ed27f74489
Hodei Otegi González <otegih@gmail.com>
2021-06-04 12:19:37 +0200

    Polloooooos

--- App/Source/Game/GoldenHen.cs
+++ App/Source/Game/GoldenHen.cs
@@ -1,6 +1,28 @@
-﻿namespace TcGame
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+
+namespace TcGame
 {
   public class GoldenHen : Enemy
   {
-  }
+        private Texture texture;
+
+        public GoldenHen()
+        {
+            texture = Resources.Texture("Textures/Enemies/Chicken02");
+            texture.Repeated = true;
+            AnimatedSprite = new AnimatedSprite(texture, 3, 1);
+            AnimatedSprite.Loop = true;
+            AnimatedSprite.FrameTime = 0.1f;
+            Position = new Vector2f(1024, 768) / 2;
+            Center();
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+        }
+    }
 }
