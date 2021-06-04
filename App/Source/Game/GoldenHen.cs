bada3ab23814d0a8224e76fa17e45fd6cc9ce444
Hodei Otegi González <otegih@gmail.com>
2021-06-04 12:43:47 +0200

    Neasd

--- App/Source/Game/GoldenHen.cs
+++ App/Source/Game/GoldenHen.cs
@@ -5,13 +5,34 @@ using System;
 
 namespace TcGame
 {
-  public class GoldenHen : Enemy
-  {
+    public class GoldenHen : Enemy
+    {
         private Texture texture;
 
         public GoldenHen()
         {
             texture = Resources.Texture("Textures/Enemies/Chicken02");
+            texture.Repeated = true;
+            AnimatedSprite = new AnimatedSprite(texture, 3, 1);
+            AnimatedSprite.Loop = true;
+            AnimatedSprite.FrameTime = 0.1f;
+            Position = new Vector2f(1024, 768) / 2;
+            Center();
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+        }
+    }
+
+    public class GoldenHenX : Enemy
+    {
+        private Texture texture;
+
+        public GoldenHenX()
+        {
+            texture = Resources.Texture("Textures/Enemies/Chicken02X");
             texture.Repeated = true;
             AnimatedSprite = new AnimatedSprite(texture, 3, 1);
             AnimatedSprite.Loop = true;
--- App/Source/Game/GreenHen.cs
+++ App/Source/Game/GreenHen.cs
@@ -5,13 +5,34 @@ using System;
 
 namespace TcGame
 {
-  public class GreenHen : Enemy
-  {
+    public class GreenHen : Enemy
+    {
         private Texture texture;
 
         public GreenHen()
         {
             texture = Resources.Texture("Textures/Enemies/Chicken03");
+            texture.Repeated = true;
+            AnimatedSprite = new AnimatedSprite(texture, 3, 1);
+            AnimatedSprite.Loop = true;
+            AnimatedSprite.FrameTime = 0.1f;
+            Position = new Vector2f(1024, 768) / 2;
+            Center();
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+        }
+    }
+
+    public class GreenHenX : Enemy
+    {
+        private Texture texture;
+
+        public GreenHenX()
+        {
+            texture = Resources.Texture("Textures/Enemies/Chicken03X");
             texture.Repeated = true;
             AnimatedSprite = new AnimatedSprite(texture, 3, 1);
             AnimatedSprite.Loop = true;
