da658f924035725ebd7d9d489a47c9e9cbfec7b6
Hodei Otegi González <otegih@gmail.com>
2021-06-04 13:25:37 +0200

    AA

--- App/Source/Game/MyGame.cs
+++ App/Source/Game/MyGame.cs
@@ -45,8 +45,21 @@ namespace TcGame
 
         }
 
+        public enum State
+        {
+            None,
+            Splash,
+            Playing,
+            Option
+        }
+
+        public State currentState = State.None;
+
         public void Init()
         {
+
+
+
             Resources.LoadResources();
 
             VideoMode videoMode = new VideoMode(1000, 649);
