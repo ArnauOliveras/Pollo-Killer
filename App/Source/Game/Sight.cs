79e3aec5f9e6a30b4d31252311a2334af297bbc2
Hodei Otegi González <otegih@gmail.com>
2021-06-07 12:42:20 +0200

    Jahjdhj

--- App/Source/Game/Sight.cs
+++ App/Source/Game/Sight.cs
@@ -8,7 +8,6 @@ namespace TcGame
     public class Sight : StaticActor
     {
         private Texture texture;
-        //private Vector2f WeaponAim1, WeaponAim2;
 
         public Sight()
         {
@@ -19,12 +18,44 @@ namespace TcGame
             Sprite = new Sprite(texture);
             Position = new Vector2f(1024, 768) / 2;
             Center();
-
         }
         public override void Update(float dt)
         {
             base.Update(dt);
-            Vector2f movimiento = new Vector2f(0.0f, 0.0f);
+            Vector2f movimiento = new Vector2f(0.0f, -2.0f);
+            Position += movimiento;
+
+            if (Position.Y <= 0)
+            {
+                Position = MyGame.leftStartPosition;
+            }
+        }
+    }
+
+    public class SightX : StaticActor
+    {
+        private Texture texture;
+
+        public SightX()
+        {
+            Layer = ELayer.Front;
+
+            texture = Resources.Texture("Textures/Mira");
+            texture.Repeated = true;
+            Sprite = new Sprite(texture);
+            Position = new Vector2f(1024, 768) / 2;
+            Center();
+        }
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+            Vector2f movimiento = new Vector2f(0.0f, -2.0f);
+            Position += movimiento;
+
+            if (Position.Y <= 0)
+            {
+                Position = MyGame.rightStartPosition;
+            }
         }
     }
 }
